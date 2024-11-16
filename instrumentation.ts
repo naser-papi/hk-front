ae3512a0cf13fb023586fca3cb5003c94c0a05a0
Naser Papi <naser.papi.dev@gmail.com>
2024-11-16 20:31:35 +0330

    some new changes

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-    console.log("process.env.NEXT_RUNTIME", process.env.NEXT_RUNTIME);
-    if (process.env.NEXT_RUNTIME === "nodejs") {
-        await import("nextinspect/tracing");
-    }
-}
