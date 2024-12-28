9127d102b71cb591b2b9fbce6c47d6c36406baec
Naser Papi <naser.papi.dev@gmail.com>
2024-12-28 23:25:32 +0330

    remove the unneeded font

--- src/components/organism/blog-detail.tsx
+++ src/components/organism/blog-detail.tsx
@@ -4,12 +4,9 @@ import { NoData, VideoPlayer } from "@/components/molecule";
 const BlogDetail = async () => {
     const info = await GetBlogDetail();
     if (!info) return <NoData />;
-    const replacedFontFS = info.firstSection.replace(
-        /font-family:IRANSansXFaNum;/g,
-        ""
-    );
+    const replacedFontFS = info.firstSection.replace(/font-family:[^;]+;/g, "");
     const replacedFontSS =
-        info.secondSection?.replace(/font-family:IRANSansXFaNum;/g, "") || "";
+        info.secondSection?.replace(/font-family:[^;]+;/g, "") || "";
     return (
         <div className={"content-detail-body"}>
             <h3>{info.subTitle}</h3>
