1668b8de87e3c2f17d1d27ed6ef644ac213b4766
Naser Papi <naser.papi.dev@gmail.com>
2025-01-03 23:01:52 +0330

    a simple cache test

--- src/constants/api-path/banners.ts
+++ src/constants/api-path/banners.ts
@@ -5,7 +5,7 @@ export const BannersAPIPath = {
         method: "GET",
         url: `api/banners?populate[blogs][populate][0]=bannerMedia&populate[event][populate][0]=bannerMedia&populate[link][populate][0]=bannerMedia&populate[service][populate][0]=bannerMedia&pagination[page]=1&pagination[pageSize]=10&sort[0]=id:desc`,
         options: {
-            next: { revalidate: 3600 },
+            next: { revalidate: 360 },
         },
     },
 } satisfies { [key: string]: IAPIInfo };
