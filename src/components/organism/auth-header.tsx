924c65267058c09385cc408a106aca72751f4a2a
Naser Papi <naser.papi.dev@gmail.com>
2025-01-06 22:34:18 +0330

    changed the auth banner

--- src/components/organism/auth-header.tsx
+++ src/components/organism/auth-header.tsx
@@ -1,32 +1,19 @@
 import Image from "next/image";
-import bigFlag from "assets/images/big-flag.jpeg";
-import NlLogo from "@/components/atom/nl-logo";
+import authBannerFull from "assets/images/auth-banner-full.png";
 
 const AuthHeader = () => {
     return (
         <header
-            className={"relative h-[140px] w-full md:h-[200px] lg:h-[270px]"}
+            className={
+                "relative mx-auto h-[220px] w-full max-w-[1024px] overflow-x-hidden md:h-[320px]"
+            }
         >
             <Image
-                src={bigFlag}
-                alt={"Holland Kadeh Banner"}
+                src={authBannerFull}
+                alt={"Holland Kade Banner"}
                 fill
                 className={"object-cover"}
             />
-            <div
-                className={
-                    "relative z-10 grid h-full w-full grid-cols-[1fr_50px] items-center gap-3 bg-[rgba(255,255,255,0.5)] p-4"
-                }
-            >
-                <h3
-                    className={
-                        "text-border-white w-[70%] text-center text-4xl font-extrabold text-primary"
-                    }
-                >
-                    Immigration To Netherlands
-                </h3>
-                <NlLogo type={"default"} />
-            </div>
         </header>
     );
 };
