93c9a27ca4c485524146efbe4f1b4ec8deccc7cf
Naser Papi <naser.papi.dev@gmail.com>
2025-02-05 17:01:29 +0330

    added the post-css nested

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        "postcss-nested": {}, // Add this line for nested CSS support
+    },
 };
 
 export default config;
