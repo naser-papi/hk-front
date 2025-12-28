5963b92a677d3102968dd3ada9452b2cf66a0bc7
naser-papi <naser.papi.dev@gmail.com>
2025-12-28 13:01:04 +0330

    refactorui/phase 3

--- src/components/molecule/form-item.tsx
+++ src/components/molecule/form-item.tsx
@@ -1,12 +1,22 @@
+import { twMerge } from "tailwind-merge";
+
 interface FormItemProps {
     label: string;
     children: React.ReactNode;
+    className?: string;
 }
 
-const FormItem = ({ label, children }: FormItemProps) => {
+const FormItem = ({ label, children, className }: FormItemProps) => {
     return (
-        <div className={"w-full"}>
-            <label className={"mb-2 inline-block bg-gray-50/50 text-black"}>
+        <div className={twMerge("w-full", className)}>
+            <label
+                className={
+                    "mb-2 inline-block px-3 py-1 rounded-md font-normal text-sm text-[var(--black-light)]"
+                }
+                style={{
+                    background: "linear-gradient(to top, var(--primary) 0%, var(--primary-light) 35%, transparent 65%)",
+                }}
+            >
                 {`${label}:`}
             </label>
             {children}
--- src/components/organism/landing-contact-form.tsx
+++ src/components/organism/landing-contact-form.tsx
@@ -5,6 +5,7 @@ import { UserMessageDto } from "@/types/dto/user-message";
 import { Button, TextBox } from "@/components/atom";
 import { SendNewMessage } from "@/services/user-messages";
 import useTranslation from "@/helpers/i18n/use-translation";
+import { FormItem } from "../molecule";
 
 const initialState = {
     fullName: "",
@@ -36,41 +37,48 @@ const LandingContactForm = () => {
             }
             onSubmit={onSubmit}
         >
-            <TextBox
-                type={"text"}
-                value={info.fullName}
-                name={"fullName"}
-                key={"fullName"}
-                placeholder={t("common.fullName")}
-                updateDto={updateDto}
-                className={"lg:col-span-2"}
-            />
-            <TextBox
-                type={"text"}
-                value={info.email}
-                name={"email"}
-                key={"email"}
-                placeholder={t("common.email")}
-                updateDto={updateDto}
-            />
-            <TextBox
-                type={"text"}
-                value={info.phoneNo}
-                name={"phoneNo"}
-                key={"phoneNo"}
-                updateDto={updateDto}
-                placeholder={t("common.phoneNo")}
-            />
-            <TextBox
-                type={"textarea"}
-                value={info.message}
-                name={"message"}
-                key={"message"}
-                updateDto={updateDto}
-                placeholder={t("common.message")}
-                rows={4}
-                className={"lg:col-span-2"}
-            />
+            <FormItem label={t("common.fullName")} className="lg:col-span-2">
+                <TextBox
+                    type={"text"}
+                    value={info.fullName}
+                    name={"fullName"}
+                    key={"fullName"}
+                    placeholder={t("common.fullName")}
+                    updateDto={updateDto}                    
+                />
+            </FormItem>
+            
+            <FormItem label={t("common.email")}>
+                <TextBox
+                    type={"text"}
+                    value={info.email}
+                    name={"email"}
+                    key={"email"}
+                    placeholder={t("common.email")}
+                    updateDto={updateDto}
+                />
+            </FormItem>
+            <FormItem label={t("common.phoneNo")}>
+                <TextBox
+                    type={"text"}
+                    value={info.phoneNo}
+                    name={"phoneNo"}
+                    key={"phoneNo"}
+                    updateDto={updateDto}
+                    placeholder={t("common.phoneNo")}
+                />
+            </FormItem>
+            <FormItem label={t("common.message")} className="lg:col-span-2">
+                <TextBox
+                    type={"textarea"}
+                    value={info.message}
+                    name={"message"}
+                    key={"message"}
+                    updateDto={updateDto}
+                    placeholder={t("common.message")}
+                    rows={4}                    
+                />
+            </FormItem>
             <Button
                 label={t("common.sendMessage")}
                 variant={"primary"}
